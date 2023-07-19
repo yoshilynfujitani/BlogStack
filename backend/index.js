4bb4fc1fa2d14df6b3ef1727a42c2c5ef9def952
yoshi.dev <fujitaniyoshilynp@gmail.com>
2023-07-19 19:45:59 +0800

    Added cookies and jsonwebtokens for login

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,17 @@ import postRoutes from "./routes/posts.js";
 import authRoutes from "./routes/auth.js";
 import usersRoutes from "./routes/users.js";
 import cors from "cors";
-
+import cookieParser from "cookie-parser";
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Replace this with the URL of your frontend
+    credentials: true, // Allow credentials (cookies) to be sent by the frontend
+  })
+);
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/posts", postRoutes);
 app.use("/api/auth", authRoutes);
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,14 +1,57 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+const Register = () => {
+  const navigate = useNavigate();
+  const [inputs, setinputs] = useState({
+    username: "",
+    password: "",
+  });
+  const [err, seterr] = useState(null);
 
-const Login = () => {
+  const handlechange = (e) => {
+    setinputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handlesubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(
+        "http://localhost:5175/api/auth/login",
+        inputs,
+        {
+          withCredentials: true,
+        }
+      );
+      console.log(res);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+      seterr(err.response.data);
+    }
+  };
+  console.log(inputs);
   return (
     <div className="auth">
       <h1>Login</h1>
       <form>
-        <input type="text" placeholder="username" />
-        <input type="password" placeholder="password" />
-        <button>Login</button>
-        <p>This is an error</p>
+        <input
+          required
+          type="text"
+          placeholder="username"
+          onChange={handlechange}
+          name="username"
+        />
+        <input
+          required
+          type="password"
+          placeholder="password"
+          onChange={handlechange}
+          name="password"
+        />
+
+        <button onClick={handlesubmit}>Register</button>
+        {err && <p>{err}</p>}
         <span>
           Don't have an account? <Link to="/register">Register</Link>
         </span>
@@ -17,4 +60,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Register;
